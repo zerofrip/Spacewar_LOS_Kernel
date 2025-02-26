d34c55bd8197effcd05632121b8cd5fe409c6993
zerofrip <zerofripside@gmail.com>
2025-02-26 15:27:09 +0900

    69_hide_stuff.patch
    https://github.com/WildPlusKernel/kernel_patches/blob/main/69_hide_stuff.patch

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -2195,6 +2195,17 @@ static int map_files_get_link(struct dentry *dentry, struct path *path)
 		path_get(path);
 		rc = 0;
 	}
+	if (vma) {
+        if (vma->vm_file) {
+            if (strstr(vma->vm_file->f_path.dentry->d_name.name, "lineage")) { 
+				rc = kern_path("/system/framework/framework-res.apk", LOOKUP_FOLLOW, path);
+			} else {
+				*path = vma->vm_file->f_path;
+				path_get(path);
+				rc = 0;
+            }
+        }
+    }
 	up_read(&mm->mmap_sem);
 
 out_mmput:
