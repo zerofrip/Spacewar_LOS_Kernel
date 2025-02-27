ad964d32f099e30d44abc10c3703c5ceaea4fe05
zerofrip <zerofripside@gmail.com>
2025-02-27 09:45:18 +0900

    Add Build Script
    https://github.com/kimocoder/android_kernel_nothing_sm7325/blob/nethunter-stable/build.sh

---
+++ build.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+#
+# kimocoder kernel builder and packer
+# 2024 - kimocoder
+#
+
+# Setup getopt.
+long_opts="regen,clean,homedir:,tcdir:"
+getopt_cmd=$(getopt -o rch:t: --long "$long_opts" \
+            -n $(basename $0) -- "$@") || \
+            { echo -e "\nError: Getopt failed. Extra args\n"; exit 1;}
+
+eval set -- "$getopt_cmd"
+
+while true; do
+    case "$1" in
+        -r|--regen|r|regen) FLAG_REGEN_DEFCONFIG=y;;
+        -c|--clean|c|clean) FLAG_CLEAN_BUILD=y;;
+        -h|--homedir|h|homedir) HOME_DIR="$2"; shift;;
+        -t|--tcdir|t|tcdir) TC_DIR="$2"; shift;;
+        --) shift; break;;
+    esac
+    shift
+done
+
+# Setup HOME dir
+if [ $HOME_DIR ]; then
+    HOME_DIR=$HOME_DIR
+else
+    HOME_DIR=$HOME
+fi
+echo -e "HOME directory is at $HOME_DIR\n"
+
+# Setup Toolchain dir
+if [ $TC_DIR ]; then
+    TC_DIR="/home/zero/work/r530567"
+else
+    TC_DIR="$HOME_DIR/tc"
+fi
+echo -e "Toolchain directory is at $TC_DIR\n"
+
+SECONDS=0 # builtin bash timer
+ZIPNAME="Uo_Spacewar_LOS_Kernel.zip"
+
+CLANG_DIR="$TC_DIR"
+AK3_DIR="$HOME/AnyKernel3"
+DEFCONFIG="spacewar_defconfig"
+
+MAKE_PARAMS="O=out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- LLVM=1 LLVM_IAS=1 \
+	CROSS_COMPILE=aarch64-linux-android-"
+
+export PATH="/home/zero/work/r530567/bin:$PATH"
+
+# Regenerate defconfig, if requested so
+if [ "$FLAG_REGEN_DEFCONFIG" = 'y' ]; then
+	make $MAKE_PARAMS $DEFCONFIG savedefconfig
+	cp out/defconfig arch/arm64/configs/$DEFCONFIG
+	echo -e "\nSuccessfully regenerated defconfig at $DEFCONFIG"
+	exit
+fi
+
+# Prep for a clean build, if requested so
+if [ "$FLAG_CLEAN_BUILD" = 'y' ]; then
+	echo -e "\nCleaning output folder..."
+	rm -rf out
+fi
+
+mkdir -p out
+make $MAKE_PARAMS $DEFCONFIG
+
+echo -e "\nStarting compilation...\n"
+make -j$(nproc --all) $MAKE_PARAMS || exit $?
+make -j$(nproc --all) $MAKE_PARAMS INSTALL_MOD_PATH=modules INSTALL_MOD_STRIP=1 modules_install
+
+kernel="out/arch/arm64/boot/Image"
+dts_dir="out/arch/arm64/boot/dts/vendor/qcom"
+
+if [ -f "$kernel" ] && [ -d "$dts_dir" ]; then
+	echo -e "\nKernel compiled succesfully! Zipping up...\n"
+	if [ -d "$AK3_DIR" ]; then
+		cp -r $AK3_DIR AnyKernel3
+		git checkout spacewar &> /dev/null
+	elif ! git clone https://github.com/zerofrip/AnyKernel3 -b spacewar_los; then
+		echo -e "\nAnyKernel3 repo not found locally and couldn't clone from GitHub! Aborting..."
+		exit 1
+	fi
+	cp $kernel AnyKernel3
+	cat $dts_dir/*.dtb > AnyKernel3/dtb
+	python3 scripts/mkdtboimg.py create AnyKernel3/dtbo.img --page_size=4096 $dts_dir/*.dtbo
+	rm -rf out/arch/arm64/boot
+	cd AnyKernel3
+	zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
+	cd ..
+	rm -rf AnyKernel3
+	echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+	echo "Zip: $ZIPNAME"
+	#curl -F "file=@${ZIPNAME}" https://oshi.at
+else
+	echo -e "\nCompilation failed!"
+	exit 1
+fi
